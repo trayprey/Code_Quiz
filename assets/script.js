9c6428345a3c192f49fcc094c7ec0cdc9852b0db
trayprey <119766277+trayprey@users.noreply.github.com>
2023-01-26 23:45:47 -0500

    added questions in .js file

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+var question1 = [
+    {
+        title: "Commonly used data types DO NOT include."
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "booleans",
+    }
+];
+
+var question2 = [
+    {
+        title: "The condition in an if/else statement is enclosed with ________."
+        choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        answer: "parenthesis",
+    }
+];
+
+var question3 = [
+    {
+        title: "Arrays in Javascript can be used to store"
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above."],
+        answer: "all of the above",
+    }
+];
+
+var question4 = [
+    {
+        title: "String values must be enclosed within ________ when being assigned to variables."
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes",
+    }
+];
+
+var question5 = [
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:"
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "CORRECT ANSWER HERE",
+    }
+];
